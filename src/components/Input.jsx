6e5c9102d2abe40e8a9e0140ad2d8f1d41c631e4
rathodashwini990 <rathodashwini990@gmail.com>
2025-04-04 12:39:55 +0530

    Created components

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Input = (props) => {
+    return (
+        <input
+            className='input-field'
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange={props.onChange}
+        />
+    );
+};
+export default Input;
